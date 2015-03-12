f0cd4aa9be648e179d44b5c862e3119ba7616204
devblah <trialgod@gmail.com>
2015-03-12 17:23:28 +0100

    test commit

--- DevBlah.SqlExpressionBuilder/DbInsertExpressionBuilder.cs
+++ DevBlah.SqlExpressionBuilder/DbInsertExpressionBuilder.cs
@@ -1,7 +1,12 @@
 ﻿
+using System.Data;
+
 namespace DevBlah.SqlExpressionBuilder
 {
-    public class DbInsertExpressionBuilder
+    public class DbInsertExpressionBuilder<TFluent, TDbParameter>
+        : IDbInsertExpressionBuilder<TFluent, TDbParameter>
+        where TFluent : DbInsertExpressionBuilder<TFluent, TDbParameter>
+        where TDbParameter : IDbDataParameter
     {
     }
 }
